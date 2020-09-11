00665551e763e988999539b510a1890495605029
rshtishi <rando.shtishi@gmail.com>
2020-09-11 16:09:29 +0200

    adding one to one table relation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,19 @@ create table if not exists person
    last_name varchar(30),
    birthdate date
 );
+
+create table if not exists product_details
+(
+  id integer primary key AUTO_INCREMENT,
+  created_by varchar(30),
+  created_on timestamp
+);
+
+create table if not exists product
+(
+	id integer primary key AUTO_INCREMENT,
+	name varchar(30),
+	details_id integer,
+	constraint details_id_fk foreign key(details_id) REFERENCES product_details(id)
+);
+
